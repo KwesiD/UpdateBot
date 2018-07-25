04d584b1461847ac409fbab5239c1d7cf38730ff
KwesiD <kfd233@nyu.edu>
2018-07-24 20:47:41 -0400

    Added a few more comments to the code. Bot now replies to posts and sends data to the database. Next step is to work on reminders then host on AWS

--- main.py
+++ main.py
@@ -6,39 +6,42 @@ import string
 import hashlib
 import time
 
-punctuation = string.punctuation.replace(">","").replace("=","").replace("!","").replace("/","")
-punctmap = str.maketrans("","",punctuation)
-get_time_seconds = lambda: int(round(time.time() * 1000)) #gets the current time in seconds
+punctuation = string.punctuation.replace(">","").replace("=","").replace("!","").replace("/","")  #A list of all standard punctuation minus a few tokens that the bot searches for 
+punctmap = str.maketrans("","",punctuation) #used later to filter punctuation from text
+get_time_seconds = lambda: int(round(time.time())) #gets the current time in seconds
 
 def parseComment(comment):
 	"""
 	Parses comment to determine what type of query to run. Does not validate the query just yet
 		-comment: The body of reddit comment object
 	Returns: object containing the keywords for the query
-	"""
-	commentTokens = comment.lower().translate(punctmap).split() #Individual tokens/terms in comment
-
-	startIndex = 0
+	""" 
+	commentTokens = comment.lower().translate(punctmap).split() #Individual tokens/terms in comment. Remove most punctuation besides (>!=/)
+	startIndex = 0 #start of query.
 	try:
 		startIndex = commentTokens.index("!updateme") #The index where !UpdateMe is found
-	except ValueError:
-		return None
+	except ValueError: #if !UpdateMe is found but not as a standalone token (ie "!UpdateMe" or !!UpdateMe versu !UpdateMe)
+		return None	   #then we return none, as this is an invalid query
 
 	sliceRange = slice(startIndex,startIndex+5) #Max query should be 5 terms, ie: !UpdateMe on user in subreddit
 
 	queryWindow = commentTokens[sliceRange] #At most, 5 tokens that comprise the query and possibly some extra text
 
 	#pad array with None if len > 5
-	len_diff = 5 - len(queryWindow) 
-	queryWindow += [None]*len_diff 
+	lenDiff = 5 - len(queryWindow) 
+	queryWindow += [None]*lenDiff 
+	for i in range(1,len(queryWindow)):
+		if(queryWindow[i] != None):
+			queryWindow[i] = queryWindow[i].replace("!","")
 	
+
 	"""
 	The summons should have the following format
 	!UpdateMe [mode] [target] (in [target2])/([clause])
 	Examples: !UpdateMe on user in subreddit | !UpdateMe when votes > 1000
 	"""
-	modes = ["on","when","help",]
-	targets = ["op","post","all","comment"] #target1 can also be a username such as u/UpdatesAssistant
+	modes = ["on","when","help",]  #what kind of query is this?
+	targets = ["op","post","all","comment"] #target can also be a username such as u/UpdatesAssistant
 
 
 	mode = None
@@ -46,6 +49,7 @@ def parseComment(comment):
 	hasIn = False
 	target2 = None
 	clause = None
+
 	if(queryWindow[1] in modes): #Checks mode
 		mode = queryWindow[1]
 		if(mode == "on"):
@@ -76,10 +80,15 @@ def parseComment(comment):
 
 	
 def validateComment(comment):
+	"""
+	Ensures that the query is valid. 
+		-comment: the object containing the parameters from the parsed comment
+	returns: True if the comment is deemed valid and false otherwise
+	"""
 	mode = comment["mode"]
 	target = comment["target"]
 	target2 = comment["target2"]
-	hasIn = comment["hasIn"]
+	hasIn = comment["hasIn"] 
 	clause = comment["clause"]
 	if(mode != None):
 		if(mode == "on" and target == None):
@@ -92,13 +101,13 @@ def validateComment(comment):
 
 
 
-def check_if_summons(comment):
-
+def checkIfSummons(comment):
 	"""
-	Determines if the comment is summoning the bot
+	Determines if the comment is summoning the bot. Not rigorous and functions
+	just as an initial filter.
 		-comment: Reddit comment object
 
-	return: True if the comment is a summons. False if not
+	return: True if the comment might be a summons. False if not
 	"""
 
 	#Summons must start with !UpdateMe
@@ -114,8 +123,7 @@ def check_if_summons(comment):
 #TODO: Make this a little more rigorous using reddit's api to determine if the user even exists
 def isUser(token):
 	"""
-	Determines if the token passed is a username
-	Simply checks if the token starts with "u/"
+	Determines if the token passed is a username. Simply checks if the token starts with "u/"
 		-token: The potential username
 
 	return: True if the token starts with "u/" and false otherwise.
@@ -128,11 +136,29 @@ def isUser(token):
 
 
 def processRequest(parsedComment,comment):
+	"""
+	Sends request to the proper function to the proper handler.
+		-parsedComment: the object containing the keywords from the comment
+		-comment: Reddit comment object
+	"""
 	if(parsedComment["mode"] == "on"):
-		onQuery(parsedComment,comment)
+		onHandler(parsedComment,comment)
 
-def onQuery(query,comment):
-	if(query["target"] == "post"):
+
+
+def onHandler(request,comment):
+	"""
+	Handler for when the user uses the "on" mode.
+	Packs comment/post information into a tuple to
+	dump into the sql table. A verification is then
+	sent to the user.
+		-request: The keywords for the !UpdateMe request
+		-coment: The reddit comment object that summoned the bot
+	
+	"""
+	print("Handling \"on\" request")
+	print(request)
+	if(request["target"] == "post"):
 		parent = comment.submission #In this case, the parent is the originating thread
 		# pprint(vars(comment))
 		# pprint(vars(parent))
@@ -151,12 +177,67 @@ def onQuery(query,comment):
 					parent_permalink,body_hash,poster,requester,
 					expiration_date,num_upvotes,num_comments
 					)
-		add_submission_to_database(params)
+
+		reply_body = (f"Hi u/{requester}! I'll make sure to remind you about u/{poster}\'s [post]({parent_permalink})!")
+
+		addSubmissionToDatabase(params) #sends data to sql server
+		replyToComment(comment,reply_body)
+
+	if(request["target"] == "comment"):
+		parent = comment.parent() #In this case, the parent is the parent comment
+		#pprint(vars(comment))
+		#pprint(vars(parent))
+		comment_id = comment.id
+		comment_type = 0 #comment
+		comment_permalink = comment.permalink
+		parent_permalink = parent.permalink
+		body_hash = hashlib.md5(parent.body.encode('utf-8')).hexdigest() #hexdigest converts into string
+		poster = parent.author.name
+		requester = comment.author.name
+		num_upvotes = parent.ups
+		print(parent.replies)
+		num_comments = len(parent.replies)
+		expiration_date = get_time_seconds() + 259200 #3 days from now
+		params = (
+			comment_id,comment_type,comment_permalink,
+					parent_permalink,body_hash,poster,requester,
+					expiration_date,num_upvotes,num_comments
+					)
+
+		reply_body = (f"Hi u/{requester}! I'll make sure to remind you about u/{poster}\'s [comment]({parent_permalink})!")
+
+		addSubmissionToDatabase(params) #sends data to sql server
+		replyToComment(comment,reply_body)
+
 		
 
-def add_submission_to_database(query_params):
-	cursor.execute(add_submission,query_params)
+def addSubmissionToDatabase(queryParams):
+	"""
+	Takes information and inserts them into the MySQL Database.
+	Database config found in sqlconfig.py
+		-queryParams: The information to be inserted into the database columns
+	"""
+	cursor.execute(add_submission,queryParams)
 	cnx.commit()
+
+def replyToComment(comment,body):
+	"""
+	Replies to the comment that summoned the bot. Attaches a footer to each reply from the bot
+		-comment: The reddit comment object that we are replying to
+		-body: The body of the reply
+	"""
+	if(body == None or body.strip() == ""):
+		raise ValueError("Message body cannot be empty!")
+
+	footer = ""  #Footer message for bot. Attached to every reply
+
+	if(comment != None):
+		print("Replied to comment",comment.id)
+		comment.reply(body)
+
+	else:
+		raise ValueError("Comment required!")
+
 
 
 # #Initialize PRAW
@@ -191,26 +272,29 @@ def add_submission_to_database(query_params):
 # quit()
 
 
-
-
-
 #Initialize PRAW
 reddit = praw.Reddit(client_id = config.client_id,
 					client_secret = config.client_secret,
-					user_agent = 'testscript by /u/cyclo_methane',
+					user_agent = 'UpdatesAssistant by /u/cyclo_methane',
 					username = config.dev_username,
 					password = config.dev_password)
 
+print("Initializing praw...")
+
 bot_account = reddit.redditor("UpdatesAssistant") #This bot's reddit account
 
+#delete all of this bots comments before testing
+for comment in bot_account.comments.new(limit=None):
+	comment.delete()
 
 #For now, we are only working with the Test Subreddit
 subreddit = reddit.subreddit('UpdateBotTest')
 for submission in subreddit.hot(): #get all of the posts in the subreddit
-	#print(submission.title)
+	print("Getting submission:",submission.title)
 	comments = submission.comments.list() #Get all the comments from the post
+	print(comments)
 	for comment in comments:
-		isSummons = check_if_summons(comment) #Check each comment to see if it is a summons
+		isSummons = checkIfSummons(comment) #Check each comment to see if it is a summons
 		if(isSummons):
 			parsedComment = parseComment(comment.body)
 			valid = False
@@ -218,109 +302,8 @@ for submission in subreddit.hot(): #get all of the posts in the subreddit
 				valid = validateComment(parsedComment)
 			if(valid):
 				processRequest(parsedComment,comment)
+			else:
+				print("Not a valid query:",comment.body)
 		else:
+			print("Not a summons:",comment.body)
 			continue  #continue if this comment does not contain a summons
-
-
-
-# {'_comments_by_id': {},
-#  '_fetched': False,
-#  '_flair': None,
-#  '_info_params': {},
-#  '_mod': None,
-#  '_reddit': <praw.reddit.Reddit object at 0x037D3E10>,
-#  'approved': False,
-#  'approved_at_utc': None,
-#  'approved_by': None,
-#  'archived': False,
-#  'author': Redditor(name='cyclo-methane'),
-#  'author_flair_background_color': None,
-#  'author_flair_css_class': None,
-#  'author_flair_richtext': [],
-#  'author_flair_template_id': None,
-#  'author_flair_text': None,
-#  'author_flair_text_color': None,
-#  'author_flair_type': 'text',
-#  'banned_at_utc': None,
-#  'banned_by': None,
-#  'can_gild': True,
-#  'can_mod_post': True,
-#  'category': None,
-#  'clicked': False,
-#  'comment_limit': 2048,
-#  'comment_sort': 'best',
-#  'content_categories': None,
-#  'contest_mode': False,
-#  'created': 1532181513.0,
-#  'created_utc': 1532152713.0,
-#  'distinguished': None,
-#  'domain': 'self.UpdateBotTest',
-#  'downs': 0,
-#  'edited': False,
-#  'gilded': 0,
-#  'hidden': False,
-#  'hide_score': False,
-#  'id': '90n7sw',
-#  'ignore_reports': False,
-#  'is_crosspostable': False,
-#  'is_original_content': False,
-#  'is_reddit_media_domain': False,
-#  'is_self': True,
-#  'is_video': False,
-#  'likes': None,
-#  'link_flair_css_class': None,
-#  'link_flair_richtext': [],
-#  'link_flair_text': None,
-#  'link_flair_text_color': 'dark',
-#  'link_flair_type': 'text',
-#  'locked': False,
-#  'media': None,
-#  'media_embed': {},
-#  'media_only': False,
-#  'mod_note': None,
-#  'mod_reason_by': None,
-#  'mod_reason_title': None,
-#  'mod_reports': [],
-#  'name': 't3_90n7sw',
-#  'no_follow': True,
-#  'num_comments': 4,
-#  'num_crossposts': 0,
-#  'num_reports': 0,
-#  'over_18': False,
-#  'parent_whitelist_status': None,
-#  'permalink': '/r/UpdateBotTest/comments/90n7sw/test1/',
-#  'pinned': False,
-#  'post_categories': None,
-#  'pwls': None,
-#  'quarantine': False,
-#  'removal_reason': None,
-#  'removed': False,
-#  'report_reasons': [],
-#  'rte_mode': 'markdown',
-#  'saved': False,
-#  'score': 1,
-#  'secure_media': None,
-#  'secure_media_embed': {},
-#  'selftext': '',
-#  'selftext_html': None,
-#  'send_replies': True,
-#  'spam': False,
-#  'spoiler': False,
-#  'stickied': False,
-#  'subreddit': Subreddit(display_name='UpdateBotTest'),
-#  'subreddit_id': 't5_m3b6p',
-#  'subreddit_name_prefixed': 'r/UpdateBotTest',
-#  'subreddit_subscribers': 1,
-#  'subreddit_type': 'private',
-#  'suggested_sort': None,
-#  'thumbnail': 'self',
-#  'thumbnail_height': None,
-#  'thumbnail_width': None,
-#  'title': 'Test1',
-#  'ups': 1,
-#  'url': 'https://www.reddit.com/r/UpdateBotTest/comments/90n7sw/test1/',
-#  'user_reports': [],
-#  'view_count': None,
-#  'visited': False,
-#  'whitelist_status': None,
-#  'wls': None}
